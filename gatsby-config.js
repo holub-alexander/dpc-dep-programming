854f88147e5f2ea3c6e1b688d350fec336dad199
Alexander Holub <holub.alexander.it@gmail.com>
2021-05-23 16:59:49 +0300

    refactor: correct info about the site, deleted comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `ДПК: отделение "Программирование в компьютерных системах"`,
+    description: `Новости, мероприятия отделения программирования, Донецкого политехнического колледжа`,
+    author: `@holub-alexander`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,9 +25,9 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     `gatsby-plugin-gatsby-cloud`,
   ],
-}
+};
